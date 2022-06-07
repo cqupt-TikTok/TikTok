fbd34dd3f6f4038b3c3a78d46a4f1448585dee86
zzyzzyzzyzz <1982452293@qq.com>
2022-06-07 12:55:42 +0800

    修正结构体tag标签

--- model/comment.go
+++ model/comment.go
@@ -8,9 +8,9 @@ import (
 // Comment 评论
 type Comment struct {
 	gorm.Model
-	VideoId uint   `gorm:"column:video_id"` //视频id作为外键
-	UserId  uint   `gorm:"column:userId"`   //用户id，作为第二外键
-	Content string `gorm:"column:content"`  //评论内容
+	VideoId uint   `gorm:"column:video_id;not null"` //视频id作为外键
+	UserId  uint   `gorm:"column:userId;not null"`   //用户id，作为第二外键
+	Content string `gorm:"column:content;type:text"` //评论内容
 }
 
 // CommentResp 响应结构体
