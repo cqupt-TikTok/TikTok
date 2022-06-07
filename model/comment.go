907285ea2476d5365405d2c2f3a274335ba4300a
zzyzzyzzyzz <1982452293@qq.com>
2022-06-07 13:22:36 +0800

    结构体tag标签更新

--- model/comment.go
+++ model/comment.go
@@ -8,9 +8,9 @@ import (
 // Comment 评论
 type Comment struct {
 	gorm.Model
-	VideoId uint   `gorm:"column:video_id"` //视频id作为外键
-	UserId  uint   `gorm:"column:userId"`   //用户id，作为第二外键
-	Content string `gorm:"column:content"`  //评论内容
+	VideoId uint   `gorm:"column:video_id;not null"` //视频id作为外键
+	UserId  uint   `gorm:"column:userId;not null"`   //用户id，作为第二外键
+	Content string `gorm:"column:content;type:text"` //评论内容
 }
 
 // CommentResp 响应结构体
@@ -27,12 +27,12 @@ func (C Comment) ToResp(UserId uint) (CR CommentResp) {
 	CR.Content = C.Content
 	CR.CreateDate = C.CreatedAt.String()
 	var U User
-	db.DB.Model(&User{}).Where("id = ?", C.UserId).First(&U)
+	db.DB.Where("id=?", C.UserId).First(&U)
 	if U.ID > 0 {
-		CR.User = U.ToResp()
+		CR.User = UserResp{}
 		CR.User.IsFollowJudge(UserId)
 	} else {
-		CR.User = UserResp{}
+		CR.User = U.ToResp()
 	}
 	return CR
 }
