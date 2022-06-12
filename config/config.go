fdc8d6a417aa157ba3df7cdabb374fb896cdbc61
zzyzzyzzyzz <93390152+zzyzzyzzyzz@users.noreply.github.com>
2022-06-12 18:57:54 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -2,16 +2,16 @@ package config
 
 //mysql数据库配置
 const (
-	HOST     = "127.0.0.1:3306" //主机地址
-	DbName   = "tiktok"         //数据库名称
-	UserName = "root"           //MySQL连接用户名
-	Password = "13896764180zy"  //连接密码
+	HOST     = "127.0.0.1:3306" 	//主机地址
+	DbName   = "tiktok"        	//数据库名称
+	UserName = "你的连接用户名"       //MySQL连接用户名
+	Password = "你的密码"  	     //连接密码
 )
 
 //七牛对象储存配置
 const (
-	AccessKey = "JdfXccVJzE9MfXrae3A-lmKvGtaF7LkW8aF3__g8"
-	SecretKey = "gtrU_VsNnxeojAMF5BvT2cfXMhTy_YYPX9-7bB9m"
-	Bucket    = "cqupt-tiktok"
-	ImgUrl    = "http://zbwzbw.top/"
+	AccessKey = "your AccessKey"
+	SecretKey = "your SecretKey"
+	Bucket    = "your Bucket"
+	ImgUrl    = "your ImgUrl"
 )
